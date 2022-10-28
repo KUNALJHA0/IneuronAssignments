1efd44eb1abfe0599921362fb35d1d52eaee4e6f
KUNALJHA0 <53056205+KUNALJHA0@users.noreply.github.com>
2022-10-28 12:05:08 +0530

    Add files via upload

---
+++ FirstAssignmentQuestion2.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class FirstAssignmentQuestion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+	    int c=1;
+	    for(int i=0;i<n;i++){
+	        
+	        for(int j=0;j<n;j++){
+	        	System.out.print(c);
+	        }
+	        c++;
+	        System.out.println();
+	    }
+	}
+
+}
